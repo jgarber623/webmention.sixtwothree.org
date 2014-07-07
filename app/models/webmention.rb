31d987a3edddc117006fb93c5b47b5279d10b604
Jason Garber <jason@sixtwothree.org>
2014-07-07 16:09:03 -0400

    Handle Mechanize errors. Fixes #8

--- app/models/webmention.rb
+++ app/models/webmention.rb
@@ -11,18 +11,23 @@ class Webmention < ActiveRecord::Base
     agent = Mechanize.new
 
     # [ ] verify that target exists and accepts webmentions
-    # [ ] if source does not link to target (410 or no link_with), delete webmention
 
-    if body = agent.get(source)
-      # If source and target are on the same domain, target should be relative
-      if URI.parse(source).host == URI.parse(target).host
-        target.sub! 'http://sixtwothree.org/', '/'
-      end
+    # If source and target are on the same domain, target should be relative
+    if URI.parse(source).host == URI.parse(target).host
+      target.sub! 'http://sixtwothree.org/', '/'
+    end
 
-      # Verify that source links to target (with or without trailing slash)
-      if body.link_with(href: %r{#{target}|#{target.sub(/.*\/+?$/, '')}}).present?
-        update_attribute(:verified_at, Time.now.utc)
-      end
+    # Verify that source links to target (with or without trailing slash)
+    body = agent.get(source)
+
+    if body.link_with(href: %r{#{target}|#{target.sub(/.*\/+?$/, '')}}).present?
+      update_attribute(:verified_at, Time.now.utc)
+    else
+      delete
+    end
+  rescue Mechanize::ResponseCodeError => err
+    case err.response_code
+      when '404', '410'; delete
     end
   end
 

9ec8fa1c0e4f2a9bc35f24e296a7d70a737c1732
Jason Garber <jason@sixtwothree.org>
2014-07-07 16:49:04 -0400

    Refactoring some verification codez.

--- app/models/webmention.rb
+++ app/models/webmention.rb
@@ -10,17 +10,12 @@ class Webmention < ActiveRecord::Base
   def verify
     agent = Mechanize.new
 
-    # [ ] verify that target exists and accepts webmentions
-
     # If source and target are on the same domain, target should be relative
     if URI.parse(source).host == URI.parse(target).host
       target.sub! 'http://sixtwothree.org/', '/'
     end
 
-    source_body = agent.get(source)
-
-    # Verify that source links to target (with or without trailing slash)
-    if source_body.link_with(href: %r{#{target}|#{target.sub(/.*\/+?$/, '')}}).present?
+    if source_links_to_target?(agent.get(source))
       update_attribute(:verified_at, Time.now.utc)
     else
       delete
@@ -33,5 +28,12 @@ class Webmention < ActiveRecord::Base
 
   def self.verify_all
     where(verified_at: nil).each(&:verify)
+  end
+
+  private
+
+  def source_links_to_target?(body)
+    # Verify that source links to target (with or without trailing slash)
+    body.link_with(href: %r{#{target}|#{target.sub(/.*\/+?$/, '')}}).present?
   end
 end
